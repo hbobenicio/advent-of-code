7cb899dbfaa9711070ea191bbcebdaa8d2e8b6ed
hbobenicio <hbobenicio@gmail.com>
2021-12-09 16:31:12 -0300

    aoc2021: day 01: part 1 and 2: fixing cobol solution and reorganizing files and folders

--- 2021/01/cobol/MAIN1.cbl
+++ 2021/01/cobol/MAIN1.cbl
@@ -1,66 +1,67 @@
       *-----------------------------------------------------------------
-      * $ cobc -Wall -Wextra -x -o a.out main.cbl
+      * $ cobc -Wall -Wextra -std=ibm-strict -fixed -g -O0 -debug \
+      *        -x -o a.out cobol/MAIN1.cbl
       * $ cat inputs/example.in.txt | ./a.out
       *-----------------------------------------------------------------
        IDENTIFICATION DIVISION.
        PROGRAM-ID.  AOC-2021-D01-1.
       * AUTHOR. HBOBENICIO.
-      * DATE-WRITTEN. 07/12/2021.
+      * DATE-WRITTEN. 09/12/2021.
       * SECURITY. NON-CONFIDENTIAL.
       *-----------------------------------------------------------------
        ENVIRONMENT DIVISION.
          INPUT-OUTPUT SECTION.
          FILE-CONTROL.
-      *    SELECT EXAMPLE-FILE ASSIGN TO 'example.txt'
-           SELECT EXAMPLE-FILE ASSIGN TO KEYBOARD
+           SELECT INPUT-FILE ASSIGN TO '/dev/stdin'
            ORGANIZATION IS LINE SEQUENTIAL.
       *-----------------------------------------------------------------
        DATA DIVISION.
 
        FILE SECTION.
-       FD  EXAMPLE-FILE.
-       01  EXAMPLE-RECORD.
-           05 X PIC 9(4).
+       FD  INPUT-FILE.
+       01  INPUT-FILE-RECORD.
+           05 NUM PIC X(4).
        
        WORKING-STORAGE SECTION.
-       01  WS-XS           PIC  9(4) OCCURS 2048 TIMES.
-       01  WS-XS-LEN       PIC  9(4) VALUE 0.
-       01  WS-EOF          PIC S9(1) VALUE 0.
-           88 WS-EOF-TRUE            VALUE 1.
-           88 WS-EOF-FALSE           VALUE 0.
-       01  WS-I            PIC  9(4) VALUE 1.
-       01  WS-COUNT        PIC  9(4) VALUE 0.
-       01  WS-PREV         PIC  9(4) VALUE 9999.
-       01  WS-SOLUTION     PIC  9(4) VALUE 0.
+       01  WS-XS.
+         05  XS-ARR         PIC 9(4) OCCURS 2048 TIMES.
+         05  XS-LEN         PIC 9(4) VALUE 0.
+       01  WS-LOOP          PIC A(1).
+         88  LOOP-BREAK              VALUE 'B'.
+         88  LOOP-CONTINUE           VALUE 'C'.
+       01  WS-AUX.
+         05  WS-I           PIC 9(4) VALUE 1.
+         05  WS-COUNT       PIC 9(4) VALUE 0.
+         05  WS-PREV        PIC 9(4) VALUE 9999.
+         05  WS-SOLUTION    PIC 9(4) VALUE 0.
       *-----------------------------------------------------------------
        PROCEDURE DIVISION.
        MAIN.
            PERFORM READ-AND-PARSE-INPUT.
            PERFORM SOLVE.
            DISPLAY WS-SOLUTION.
+           MOVE 0 TO RETURN-CODE.
            GOBACK.
 
        READ-AND-PARSE-INPUT.
-           OPEN INPUT EXAMPLE-FILE.
-           PERFORM UNTIL WS-EOF = 1
-              READ EXAMPLE-FILE
-                 AT END
-                    MOVE 1 TO WS-EOF
-                 NOT AT END
-                    MOVE X TO WS-XS(WS-XS-LEN + 1)
-                    SET WS-XS-LEN UP BY 1
+           OPEN INPUT INPUT-FILE.
+           PERFORM UNTIL LOOP-BREAK
+              READ INPUT-FILE AT END SET LOOP-BREAK TO TRUE
+              NOT AT END
+                 MOVE NUM TO XS-ARR(XS-LEN + 1)
+                 SET XS-LEN UP BY 1
               END-READ
            END-PERFORM.
-           CLOSE EXAMPLE-FILE.
+           CLOSE INPUT-FILE.
 
        SOLVE.
            MOVE 0 TO WS-COUNT.
            MOVE 1 TO WS-I.
-           PERFORM UNTIL WS-I > WS-XS-LEN
-              IF WS-XS(WS-I) > WS-PREV
+           PERFORM UNTIL WS-I > XS-LEN
+              IF XS-ARR(WS-I) > WS-PREV
                  SET WS-COUNT UP BY 1
               END-IF
-              MOVE WS-XS(WS-I) TO WS-PREV
+              MOVE XS-ARR(WS-I) TO WS-PREV
               SET WS-I UP BY 1
            END-PERFORM.
            MOVE WS-COUNT TO WS-SOLUTION.
