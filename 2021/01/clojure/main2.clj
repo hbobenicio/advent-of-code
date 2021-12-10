f7cc09164c1d119b1a835b2aaeead326c8b341d4
hbobenicio <hbobenicio@gmail.com>
2021-12-10 06:00:39 -0300

    aoc2021: day 01: part 1: solution in clojure; part 2: drafting

---
+++ 2021/01/clojure/main2.clj
@@ -0,0 +1,30 @@
+(ns main2
+    (:import [java.io BufferedReader])
+    (:gen-class))
+
+(defn parse-line
+    "Parses a single line of input"
+    [line]
+    (Integer/parseInt line))
+
+(defn parse-lines
+    "Parses all lines of input. Just calls parse-line for all of them"
+    [lines]
+    (map parse-line lines))
+
+(defn read-lines
+    "Read lines from stdin as a lazy sequence of strings"
+    []
+    (line-seq (BufferedReader. *in*)))
+
+(defn solve
+    "Solve the logic of the problem"
+    [x & xs]
+    
+    )
+
+(defn -main [& args]
+    (let [xs (parse-lines (read-lines))]
+        (println (solve xs))))
+
+(-main)
